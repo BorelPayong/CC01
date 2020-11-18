02a77d16783fb201573f6f19ddaf90196fcb4f1d
dane8701 <68973297+dane8701@users.noreply.github.com>
2020-11-18 21:59:02 +0100

    mise a jour du fichier BO... J'ai ajouté les attributs, le constructeur vide et le constructeur ainsi que les methodes equal et gethashcode

---
+++ CC01.BO/Etudiant.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.BO
+{
+    [Serializable]
+    public class Etudiant
+    {
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public DateTime Born { get; set; }
+        public string Lieu { get; set; }
+        public int Identifiant { get; set; }
+        public double Contact { get; set; }
+        public string Email { get; set; }
+        public byte[] Picture { get; set; }
+
+        public Etudiant() // Pour la serialisation
+        {
+              
+        }
+
+        public Etudiant(string lastname, string firstname, DateTime born, string lieu, int identifiant, double contact, string email, byte[] picture)
+        {
+            Lastname = lastname;
+            Firstname = firstname;
+            Born = born;
+            Lieu = lieu;
+            Identifiant = identifiant;
+            Contact = contact;
+            Email = email;
+            Picture = picture;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Etudiant etudiant &&
+                   Identifiant == etudiant.Identifiant;
+        }
+
+        public override int GetHashCode()
+        {
+            return 574969646 + Identifiant.GetHashCode();
+        }
+    }
+}
